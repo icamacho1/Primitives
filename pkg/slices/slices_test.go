4bfdf0cda18ff60caa0087c6bea51f076ab1538a
Iker Camacho <iker.camacho@onum.com>
2025-05-26 07:29:47 +0000

    Get more tests

--- pkg/slices/slices_test.go
+++ pkg/slices/slices_test.go
@@ -20,4 +20,19 @@ func TestSlice(t *testing.T) {
 	set, ok := s.Uniq()
 	assert.True(t, ok)
 	assert.Equal(t, 5, set.Len())
+
+	s = s.Pop(2)
+	assert.Equal(t, 5, s.Len())
+	assert.Equal(t, "Hiroshima", s.Get(2))
+
+	s2 := s.ForEach(func(item string, i int) string {
+		return item + " City"
+	})
+	assert.Equal(t, "Tokyo City", s2.Get(0))
+	assert.Equal(t, "Tokyo", s.Get(0))
+
+	s.Map(func(item string, i int) string {
+		return item + " Prefecture"
+	})
+	assert.Equal(t, "Tokyo Prefecture", s.Get(0))
 }
